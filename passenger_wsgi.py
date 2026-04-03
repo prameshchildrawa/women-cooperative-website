c6c4b580d22e5f9cc87efde5934c4c3e0f26a233
MSMC Admin <admin@msmc.com.np>
2026-04-03 13:37:33 +0545

    Fix cPanel path error - use msmc subdirectory instead of public_html

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,21 +1,24 @@
 """
 Passenger WSGI Application Entry Point for cPanel Hosting (babal.host)
 This file tells the server how to run your Django application
+
+IMPORTANT: This configuration assumes your app is in /home/USERNAME/msmc
+NOT in public_html (cPanel doesn't allow public_html as Python app root)
 """
 
 import os
 import sys
 
 # ============================================
-# IMPORTANT: UPDATE THESE VALUES
+# IMPORTANT: UPDATE THIS VALUE
 # ============================================
 CPANEL_USERNAME = 'your_cpanel_username'  # <-- CHANGE THIS to your cPanel username
 
 # ============================================
 # PATH CONFIGURATION
 # ============================================
-# Project directory (where manage.py is located)
-PROJECT_ROOT = f'/home/{CPANEL_USERNAME}/public_html'
+# Project directory - use 'msmc' subdirectory (NOT public_html)
+PROJECT_ROOT = f'/home/{CPANEL_USERNAME}/msmc'
 
 # Add project to Python path
 if PROJECT_ROOT not in sys.path:
@@ -24,12 +27,12 @@ if PROJECT_ROOT not in sys.path:
 # ============================================
 # VIRTUAL ENVIRONMENT
 # ============================================
-# cPanel Python App virtual environment path
-VENV_PATH = f'/home/{CPANEL_USERNAME}/virtualenv/public_html/3.11/bin/activate_this.py'
+# cPanel Python App virtual environment path (for 'msmc' app)
+VENV_PATH = f'/home/{CPANEL_USERNAME}/virtualenv/msmc/3.11/bin/activate_this.py'
 
 # Alternative paths (depending on cPanel configuration)
-VENV_PATH_ALT1 = f'/home/{CPANEL_USERNAME}/virtualenv/public_html/3.11/bin/activate'
-VENV_PATH_ALT2 = f'/home/{CPANEL_USERNAME}/virtualenv/public_html/bin/activate_this.py'
+VENV_PATH_ALT1 = f'/home/{CPANEL_USERNAME}/virtualenv/msmc/3.11/bin/activate'
+VENV_PATH_ALT2 = f'/home/{CPANEL_USERNAME}/virtualenv/msmc/bin/activate_this.py'
 
 # Activate virtual environment
 def activate_virtualenv():
@@ -46,7 +49,7 @@ def activate_virtualenv():
                 continue
     
     # If no activation file found, at least try to add site-packages
-    site_packages = f'/home/{CPANEL_USERNAME}/virtualenv/public_html/3.11/lib/python3.11/site-packages'
+    site_packages = f'/home/{CPANEL_USERNAME}/virtualenv/msmc/3.11/lib/python3.11/site-packages'
     if os.path.exists(site_packages) and site_packages not in sys.path:
         sys.path.insert(0, site_packages)
     
